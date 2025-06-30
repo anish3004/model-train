8454ce20f9860db007621211e959cfab5fa54365
anish3004 <anishrout3004@gmail.com>
2025-06-30 16:25:11 +0530

    Update snowflake_config.py

--- snowflake_config.py
+++ snowflake_config.py
@@ -2,11 +2,11 @@ import snowflake.connector
 
 def get_connection():
     conn = snowflake.connector.connect(
-        user='Anish3004',
-        password='anishrout3004BDS0112',
-        account='LTSFLEG-MO32405',  
-        warehouse='ML_WH',
-        database='ML_PROJECT',
-        schema='FEATURES'
+        user='YOUR_USERNAME',
+        password='YOUR_PASSWORD',
+        account='YOUR_ACCOUNT',
+        warehouse='YOUR_WAREHOUSE',
+        database='YOUR_DATABASE',
+        schema='YOUR_SCHEMA'
     )
     return conn
